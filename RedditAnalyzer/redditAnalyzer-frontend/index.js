a4b7ab8a1d5e72de9ee62b1730c81dcd875e60c4
Andy Read <andyreadpnw@gmail.com>
2019-12-03 18:45:52 -0800

    additional element manipulation

--- RedditAnalyzer/redditAnalyzer-frontend/index.js
+++ RedditAnalyzer/redditAnalyzer-frontend/index.js
@@ -1,44 +1,35 @@
 
 
-let account_name = "spez"
-const account_URL = `https://www.reddit.com/user/${account_name}/comments/.json`
+let sample_account_name = "spez"
+const sample_account_URL = `https://www.reddit.com/user/${sample_account_name}/comments/.json`
 
-const postsPerRequest = 100;
-const maxPostsToFetch = 500;
+const postsPerRequest = 5;
+const maxPostsToFetch = 10;
 const maxRequests = maxPostsToFetch / postsPerRequest
 
-const responses = [];
+let reRankArr = [];
+
+let responses = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
-    fetchData();
     login();
 
 
-function fetchData(){
-    fetch(account_URL)
-      .then(res => res.json())
-      .then(json => displayJson(json))
-  
-  }
-  
-  function displayJson(json){
-      console.log(json)
-  }
-
   function login(){
-        const loginContainer = document.getElementById("2") 
-        let taco = document.getElementById("1")
+        const loginContainer = document.getElementById("2a") 
+        let taco = document.getElementById("1a")
         taco.addEventListener('click', event => {
             event.preventDefault();
             console.dir(loginContainer)
             loginContainer.style.display = "none";
-            handleSubmit();
         })
     }
 
 
     const handleSubmit = e => {
+      responses = [];
+
         event.preventDefault();
         const userInput = document.getElementById('user').value;
         fetchPosts(userInput);
@@ -50,7 +41,7 @@ function fetchData(){
             afterParam ? '&after=' + afterParam : ''
           }`
         );
-        console.log(response)
+        // console.log(response)
         const responseJSON = await response.json();
         responses.push(responseJSON);
       
@@ -62,12 +53,16 @@ function fetchData(){
       };
       
       const parseResults = responses => {
-        const allPosts = [];
-      
+        let allPosts = [];
+
         responses.forEach(response => {
           allPosts.push(...response.data.children);
         });
-      
+        
+        ////////////////send the posts to analyzePosts function
+        analyzePosts(allPosts)
+        ///////////////////////////////////////////////
+
         statsByUser = {};
       
         allPosts.forEach(({ data: { author, score } }) => {
@@ -88,6 +83,8 @@ function fetchData(){
         const sortedList = userList.sort((userA, userB) => userB.score - userA.score);
       
         displayRankings(sortedList);
+        allPosts = [];
+
       };
       
       const displayRankings = sortedList => {
@@ -96,13 +93,128 @@ function fetchData(){
           rank = i + 1;
           const userCard = document.createElement('a');
           userCard.href = `https://www.reddit.com/user/${username}`;
+          userCard.hidden = true;
           userCard.classList.add('user-card');
-          userCard.innerText = `${rank}. ${username} - ${postCount} post(s) - ${score} point(s)`;
+          userCard.innerText = `${rank}. ${username} = ${postCount} post(s) - ${score} points`;
       
           container.appendChild(userCard);
         });
+        sortedList = [];
+
+        //clear reRank array to preprare for a new re-rank
+        reRankArr = [];
+        reRank();
+  
       };
-      
+      ///////////////////////////////////////////////////////////////////
       const userSelectForm = document.getElementById('user-select-form');
       userSelectForm.addEventListener('submit', handleSubmit);
+      //////////////////////////////////////////////////////////////////
+
+      const reRank = () => {
+    
+        let userCardRerank = document.getElementsByClassName('user-card')
+       
+        
+        for(let i = 0; userCardRerank.length > i; i++){
+            
+          let lopOff = userCardRerank[i].innerText;
+
+          const scoreGetter = /(\d+)(?!.*\d)/g;
+          const scoreString = lopOff.match(scoreGetter)
+          let scoreInt = parseInt(scoreString)
+
+          const commentsGetter = /\s+0*(\d+)/
+          const commentsString = lopOff.match(commentsGetter)
+          let commentsInt = parseInt(commentsString)
+
+
+          const userGetter = /(?<=^\S*\s+)\S+/
+          const userString = lopOff.match(userGetter)
+
+
+          let instanceArr = [userString[0], commentsInt, scoreInt]
+
+          reRankArr.push(instanceArr) 
+          }
+
+
+          setTimeout(function(){       
+            let elements = document.getElementsByClassName('user-card');
+            while(elements.length > 0){
+              elements[0].parentNode.removeChild(elements[0]);
+          }}, 1000);
+
+          let sortedArray = reRankArr.sort(function(a, b) {
+            return b[2] - a[2];
+          });
+
+        setTimeout(function(){ 
+        for(let j = 0; sortedArray.length > j; j++){
+
+          ranker = j + 1;
+          const redditCard = document.createElement('a');
+          const redditCardBreak = document.createElement('br');
+          redditCard.href = `https://www.reddit.com/user/${sortedArray[j][0]}`;
+          redditCard.classList.add('user-card');
+          redditCard.innerText = `${ranker}. ${sortedArray[j][0]} = ${sortedArray[j][1]} post(s) - ${sortedArray[j][2]} points`;
+
+          const container = document.getElementById('results-container');
+          container.append(redditCard, redditCardBreak)
+        }}, 3000)
+      }
+
+      const analyzePosts = (allPosts) => {
+       
+        let postInstanceArr = [];
+
+        for(let d = 0; allPosts.length > d; d++){
+          let body = allPosts[d].data.body
+          let score = allPosts[d].data.score
+          let totalAwards = allPosts[d].data.total_awards_recieved
+          let num_comments = allPosts[d].data.num_comments
+          let link_permalink = allPosts[d].data.link_permalink
+          let downs = allPosts[d].data.downs
+          let ups = allPosts[d].data.ups
+          let link_url = allPosts[d].data.link_url
+
+          let thisPost = [score, body, totalAwards, num_comments, link_permalink, downs, ups, link_url]
+          postInstanceArr.push(thisPost)
+
+        }
+
+        console.log(postInstanceArr)
+
+        numCommentsSort(postInstanceArr);
+
+        downsSort(postInstanceArr);
+
+        upsSort(postInstanceArr);
+      }
+
+      const numCommentsSort = (postInstanceArr) => {
+        let numCommentsSort = [];
+        numCommentsSort = postInstanceArr.sort(function(a, b) {
+          return b[3] - a[3];
+        });
+        console.log(numCommentsSort)
+      }
+
+      const downsSort = (postInstanceArr) => {
+        let downsSort = [];
+        downsSort = postInstanceArr.sort(function(a, b) {
+          return b[6] - a[6];
+        });
+        console.log(downsSort)
+      }
+
+      const upsSort = (postInstanceArr) => {
+        let upsSort = [];
+        upsSort = postInstanceArr.sort(function(a, b) {
+          return b[7] - a[7];
+        });
+        console.log(upsSort)
+      }
+
+
     })
