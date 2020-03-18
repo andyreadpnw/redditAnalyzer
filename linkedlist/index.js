55b4f9bc4018a0d26ead5cc52f581040b78e4804
Andy Read <andyreadpnw@gmail.com>
2020-03-18 15:12:57 -0700

    first commit

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -30,12 +30,84 @@ function linkedList() {
     length++;
   };
 
-  this.remove = function(element){
-      
-  }
-  // this.isempty
-  // this.indexOf
-  // this.elementAt
-  // this.addAt
-  // this.removeAt
+  this.remove = function(element) {
+    let currentNode = head;
+    let previousNode;
+    if ((currentNode.element = element)) {
+      head = currentNode.next;
+    } else {
+      while (currentNode != element) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+    length--;
+  };
+
+  this.isEmpty = function() {
+    return length === 0;
+  };
+
+  this.indexOf = function(element) {
+    let currentNode = head;
+    let index = -1;
+
+    while (currentNode) {
+      index++;
+      if ((currentNode.element = element)) {
+        return index;
+      }
+      currentNode = currentNode.next;
+    }
+  };
+
+  this.elementAt = function(index) {
+    let currentNode = head;
+    let count = 0;
+    while (count < index) {
+      count++;
+      currentNode = currentNode.next;
+    }
+    return currentNode.element;
+  };
+
+  this.addAt = function(index, element) {
+    let node = new Node(element);
+
+    let currentNode = head;
+    let previousNode;
+    let currentIndex = 0;
+
+    if (index > length) {
+      return false;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentIndex;
+        currentNode = currentNode.next;
+      }
+      node.next = currentNode;
+      previousNode.next = node;
+    }
+    length++;
+  };
+  this.removeAt = function(index, element) {
+    let currentNode = head;
+    let previousNode;
+    let currentIndex = 0;
+
+    if (index > length) {
+      return false;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+    length--;
+    return currentNode.element;
+  };
 }
