64ede02a5bbcb23d0327cabd270ac251fe7d7b5a
Andy Read <andyreadpnw@gmail.com>
2020-04-17 11:27:39 -0700

    additional algos

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -420,35 +420,39 @@ let intToRoman = function(num) {
 
 //////////////////////////////
 
-var letterCombinations = function(digits) {
-    if(digits.length == 0) return [];
-      const phone = {
-     '2': ['a', 'b', 'c'],
-     '3': ['d', 'e', 'f'],
-     '4': ['g', 'h', 'i'],
-     '5': ['j', 'k', 'l'],
-     '6': ['m', 'n', 'o'],
-     '7': ['p', 'q', 'r', 's'],
-     '8': ['t', 'u', 'v'],
-     '9': ['w', 'x', 'y', 'z']
-   };
+// var letterCombinations = function(digits) {
+//     if(digits.length == 0) return [];
+//       const phone = {
+//      '2': ['a', 'b', 'c'],
+//      '3': ['d', 'e', 'f'],
+//      '4': ['g', 'h', 'i'],
+//      '5': ['j', 'k', 'l'],
+//      '6': ['m', 'n', 'o'],
+//      '7': ['p', 'q', 'r', 's'],
+//      '8': ['t', 'u', 'v'],
+//      '9': ['w', 'x', 'y', 'z']
+//    };
    
-   let output = [];
+//    let output = [];
    
-   function backtrack(combination, nums){
-     if(nums.length == 0) output.push(combination);
-     else {
-       let n = nums.slice(0, 1);
-       let letters = phone[n];
-       for(let i in letters){
-         let letter = letters[i]
-         backtrack(combination + letter, nums.slice(1))
-       }
-     }
-   }
+//    function backtrack(combination, nums){
+//      if(nums.length == 0) output.push(combination);
+//      else {
+//        let n = nums.slice(0, 1);
+//        let letters = phone[n];
+//        for(let i in letters){
+//          let letter = letters[i]
+//          backtrack(combination + letter, nums.slice(1))
+//        }
+//      }
+//    }
  
-   if(digits.length != 0){
-     backtrack('', digits);
-     return output;
-   }
- }
+//    if(digits.length != 0){
+//      backtrack('', digits);
+//      return output;
+//    }
+//  }
+
+
+
+///////////////////
